946b2d5c61aafbfdc24f5c10aeeb4dbacab1ce9f
selametsamli <selamet96@gmail.com>
2018-06-18 18:21:08 +0300

    admin panel customization

--- blog/article/admin.py
+++ blog/article/admin.py
@@ -5,5 +5,17 @@ from .models import Article
 
 # Register your models here.
 
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
 
-admin.site.register(Article)
+    list_display = ["title","author","created_date"]
+
+    list_display_links = ["title","created_date","author"]
+
+    search_fields = ["title"]
+
+    list_filter = ["created_date"]
+    class Meta:
+        model = Article
+
+
