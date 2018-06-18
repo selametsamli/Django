8d887fb6a87261235473a0232b2608a14e48b9a4
selametsamli <selamet96@gmail.com>
2018-06-19 00:29:39 +0300

    user app, register

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', views.index, name ="index"),
     path('about/', views.about, name ="about"),
     path('articles/', include("article.urls")),
+    path('user/', include("user.urls")),
 
 
 
